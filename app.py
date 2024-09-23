221b02b9180a82e2dffc46e51a3175a309fa1b3e
pinaki29uk <pinaki29uk@gmail.com>
2024-09-23 15:32:42 +0000

    23-09-2024

--- app.py
+++ app.py
@@ -34,12 +34,8 @@ retrieved_secret = client.get_secret(sqluser_secretname)
 
 sqluser = retrieved_secret.value
 
-print(f"sqluser name is {sqluser}.")
-
 retrieved_secret = client.get_secret(sqlpass_secretname)
 sqlpassword = retrieved_secret.value
-
-print(f"sqlpassword is {sqlpassword}.")
 
 connection_string = str("Driver={ODBC Driver 17 for SQL Server};Server=tcp:"+os.environ['SQLSERVER_NAME']+",1433;Database="+os.environ['DBNAME']+";Uid="+sqluser+";")+"Pwd="+sqlpassword+";Encrypt=yes;TrustServerCertificate=no;Connection Timeout=60;"
 
